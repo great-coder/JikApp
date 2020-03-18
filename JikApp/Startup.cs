6c2fa1e4b7548d31ebeed43fa02a755a379a5f5f
Coder the Great <m_dehghan9112@outlook.com>
2020-03-18 18:47:50 +0330

    Update Startup to enable usage of EF_Model everywhere ...

--- JikApp/Startup.cs
+++ JikApp/Startup.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -76,6 +69,8 @@ namespace JikApp
                 endpoints.MapFallbackToPage("/_Host");
                 endpoints.MapHub<ChatHub>("/hubs/chat");
             });
+
+            EF_Model.Initialize_DbContext_in_Startup(app.ApplicationServices);
         }
     }
 }
