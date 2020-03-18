fc92c0d5b0422dabdd717c41d280d67b5cc819e6
Coder the Great <m_dehghan9112@outlook.com>
2020-03-18 18:45:32 +0330

    Jik Data Model ...

---
+++ JikApp/Data/Jik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JikApp.Data
+{
+    public class Jik
+    {
+        [Key]
+        public string JikId { get; set; } = KeyGenerator.CreateId();
+
+        [Required]
+        public string SenderId { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Created_At { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
