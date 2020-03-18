cc6f295f9dd1b08791323d7295685c5706b41008
Coder the Great <m_dehghan9112@outlook.com>
2020-03-18 18:45:06 +0330

    Update ApplicationDbContext to support auto generating id for jiks and add DbSet of jiks ...

--- JikApp/Data/ApplicationDbContext.cs
+++ JikApp/Data/ApplicationDbContext.cs
@@ -1,16 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JikApp.Data
 {
+    public class KeyGenerator
+    {
+        public static string CreateId()
+        {
+            int maxSize = 128;
+            char[] chars = new char[62];
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+
+    public static class EF_Model
+    {
+        public static ApplicationDbContext dbContext = null;
+
+        public static void Initialize_DbContext_in_Startup(IServiceProvider serviceProvider)
+        {
+            IServiceScope serviceScope = serviceProvider.GetService<IServiceScopeFactory>().CreateScope();
+            dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+        }
+    }
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Jik> Jiks { get; set; }
     }
 }
