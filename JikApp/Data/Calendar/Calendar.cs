704df3001111a39948f425a8c126e07a2668a9a2
Coder the Great <m_dehghan9112@outlook.com>
2020-03-22 13:51:57 +0430

    Data model for my personal calendar

---
+++ JikApp/Data/Calendar/Calendar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JikApp.Data.Calendar
+{
+    public enum TYPE
+    {
+        WORK_DAY,
+        DAY_OFF
+    }
+
+    public class Event
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long EventId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Title { get; set; }
+
+        public TYPE EventType { get; set; }
+    }
+}
